14bd03ff795b5146563a9946d80bb99475447f24
sanidhya-singh <sanidhya235@gmail.com>
2015-07-29 16:57:36 +0530

    Minor changes to Post page item

--- client/templates/posts/post_page_item.js
+++ client/templates/posts/post_page_item.js
@@ -9,6 +9,80 @@ Template.postPageItem.helpers({
     } else {
       return 'disabled';
     }
+  },
+  stringifyDate: function(date) {
+    var split = date.split("/");
+    var returnString = split[0] + ' ';
+    /*switch(parseInt(split[0])) {
+      case 1:
+              returnString += split[0] + 'st ';
+              break;
+      case 2:
+              returnString += split[0] + 'nd ';
+              break;
+      case 3:
+              returnString += split[0] + 'rd ';
+              break;
+      case 21:
+              returnString += split[0] + 'st ';
+              break;
+      case 22:
+              returnString += split[0] + 'nd ';
+              break;
+      case 23:
+              returnString += split[0] + 'rd ';
+              break;
+      case 31:
+              returnString += split[0] + 'st ';
+              break;
+      default:
+              returnString += split[0] + 'th ';
+              break;
+    }*/
+    switch(parseInt(split[1])) {
+      case 1:
+              returnString+= 'January ';
+              break;
+      case 2:
+              returnString+= 'February ';
+              break;
+      case 3:
+              returnString+= 'March ';
+              break;
+
+              case 4:
+                      returnString+= 'April ';
+                      break;
+                      case 5:
+                              returnString+= 'May ';
+                              break;
+                              case 6:
+                                      returnString+= 'June ';
+                                      break;
+                                      case 7:
+                                              returnString+= 'July ';
+                                              break;
+                                              case 8:
+                                                      returnString+= 'August ';
+                                                      break;
+                                                      case 9:
+                                                              returnString+= 'September ';
+                                                              break;
+                                                              case 10:
+                                                                      returnString+= 'October ';
+                                                                      break;
+                                                                      case 11:
+                                                                              returnString+= 'November ';
+                                                                              break;
+                                                                              case 12:
+                                                                                      returnString+= 'December ';
+                                                                                      break;
+                                                                                      default:
+                                                                                              break;
+
+    }
+    returnString += split[2];
+    return returnString
   }
 });
 
