4895f885ef54c9641da8704d6370e0f8628f5912
sanidhya-singh <sanidhya235@gmail.com>
2015-08-06 16:54:25 +0530

    Release 6/8
    - removed Google accounts package
    - added smtp for email verification and password reset
    - updated the posts list layout
    - updates to post page item

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -10,32 +10,32 @@ Template.postsList.onRendered(function () {
       var $node = $(node), $next = $(next);
       var oldTop = $node.offset().top;
       var height = $(node).outerHeight(true);
-      
+
       // find all the elements between next and node
       var $inBetween = $(next).nextUntil(node);
       if ($inBetween.length === 0)
         $inBetween = $(node).nextUntil(next);
-      
+
       // now put node in place
       $(node).insertBefore(next);
-      
+
       // measure new top
       var newTop = $(node).offset().top;
-      
+
       // move node *back* to where it was before
       $(node)
         .removeClass('animate')
         .css('top', oldTop - newTop);
-      
+
       // push every other element down (or up) to put them back
       $inBetween
         .removeClass('animate')
         .css('top', oldTop < newTop ? height : -1 * height)
-        
-      
+
+
       // force a redraw
       $(node).offset();
-      
+
       // reset everything to 0, animated
       $(node).addClass('animate').css('top', 0);
       $inBetween.addClass('animate').css('top', 0);
@@ -45,5 +45,19 @@ Template.postsList.onRendered(function () {
         $(this).remove();
       });
     }
+  }
+});
+
+Template.postsList.onCreated(function() {
+  if (Accounts._verifyEmailToken) {
+    Accounts.verifyEmail(Accounts._verifyEmailToken, function(err) {
+      if (err != null) {
+        if (err.message = 'Verify email link expired [403]') {
+          console.log('Sorry this verification link has expired.')
+        }
+      } else {
+        console.log('Thank you! Your email address has been confirmed.')
+      }
+    });
   }
 });
---
+++ server/server.js
@@ -0,0 +1,19 @@
+Accounts.config({
+  sendVerificationEmail: true,
+  forbidClientAccountCreation: false
+});
+
+Accounts.validateLoginAttempt(function(attempt){
+  if (attempt.user && attempt.user.emails && !attempt.user.emails[0].verified ) {
+    throw new Meteor.Error(400, 'Please verify your email');
+    return false; // the login is aborted
+  }
+  return true;
+});
+
+Accounts.validateNewUser(function (user) {
+    if (user.emails[0].address.indexOf("snu.edu.in") != -1) {
+      return true;
+    }
+    throw new Meteor.Error(1, "You are not a valid user of this application.");
+});
